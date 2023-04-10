aac7e2f3a8e9670a6669cd7aef343ef003f9713d
yuler <is.yuler@gmail.com>
2023-04-10 21:27:33 +0800

    build: add rem to rpx in unocss

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,9 +1,30 @@
-import {defineConfig, presetUno} from 'unocss'
+import {Preset, defineConfig, presetUno} from 'unocss'
 
 export default defineConfig({
-  presets: [presetUno({preflight: false})],
+  presets: [presetUno({preflight: false}), presetRemTorpx()],
   preflights: [],
   shortcuts: [],
   blocklist: ['container'],
   safelist: 'm-auto invisible'.split(' '),
 })
+
+// https://unocss.dev/config/presets
+function presetRemTorpx(): Preset {
+  const remRE = /(-?[\.\d]+)rem/g
+
+  return {
+    name: 'rem-to-rpx',
+    postprocess: util => {
+      util.entries.forEach(i => {
+        const value = i[1]
+        if (value && typeof value === 'string' && remRE.test(value)) {
+          i[1] = rem2rpx(value)
+        }
+      })
+    },
+  }
+}
+
+function rem2rpx(value: string) {
+  return `${+value.slice(0, -3) * 16 * 2}rpx`
+}

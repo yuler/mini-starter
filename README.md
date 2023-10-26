dddbe6293ea224d413d9c3688f0a9a539475c1de
yuler <is.yuler@gmail.com>
2023-10-27 01:00:27 +0800

    fix: enhance-page onShow function & update README.md

--- README.md
+++ README.md
@@ -9,22 +9,24 @@
 - 🎨 Integration UnoCSS
 - 🛠️ Build System with [scripts](./scripts/)
 - 📣 Wrap a event emitter power by [mitt](https://github.com/developit/mitt)
-
-## Scripts
-
-- `ide:*` forward to [WeChat Devtools CLI](https://developers.weixin.qq.com/miniprogram/dev/devtools/cli.html)
-- `build` compiles the source code to the `dist` directory
-- `dev` runs with watch mode
-- `upload` uploads the code
+- 📦 Provide a [`enhancePage`](./src/enhance-page.ts) function to wrap the original [`Page`](https://developers.weixin.qq.com/miniprogram/dev/reference/api/Page.html)
 
 ## Usage
 
 ```bash
 gh repo create --template yuler/mini-starter
+cp .env.example .env
 ```
 
 - Change the `appid` in [project.config.json](./project.config.json)
 - Download `private.${appid}.key` file from [Weixin Official Accounts Platform](https://mp.weixin.qq.com/wxamp/devprofile/get_profile)
+
+## Scripts
+
+- `ide:*` forward to [WeChat Devtools CLI](https://developers.weixin.qq.com/miniprogram/dev/devtools/cli.html)
+- `build` compile the source code to the `dist` directory
+- `dev` run with watch mode for development
+- `upload` upload the code via [miniprogram-ci](https://www.npmjs.com/package/miniprogram-ci)
 
 ## Related
 
--- src/enhance-page.ts
+++ src/enhance-page.ts
@@ -23,13 +23,15 @@ type MPPageOptions<
   }) &
   ThisType<MP.Page.Instance<TData & Data, TCustom & Option>>
 
-export default function enchangePage<
+export default function enhancePage<
   T extends MP.Page.DataOption,
   U extends MP.Page.CustomOption,
 >(options: MPPageOptions<T, U>) {
   let onLoadComplete = false
-  // Intercept `onLoad`
   const originOnLoad = options.onLoad
+  /**
+   * Intercept the original `onLoad` method, for handle common logic. ex: `wx.login`
+   */
   const onLoad: MP.Page.ILifetime['onLoad'] = async function (
     this: any,
     query,
@@ -67,23 +69,25 @@ export default function enchangePage<
     await originOnLoad?.call(this, query)
     onLoadComplete = true
     $app.$log('enhancePage', '#onLoad complete')
+  }
 
-    // Intercept `onShow`, wait for `onLoad` to complete
-    const originOnShow = options.onShow
-    const onShow: MP.Page.ILifetime['onShow'] = async function (this: any) {
-      // polling until `onLoad` complete
-      if (!onLoadComplete) {
-        setTimeout(() => {
-          onShow()
-        }, 100)
-        return
-      }
-      const $app = getApp<IApp>()
-
-      $app.$log('enhancePage', '#onShow')
-      await originOnShow?.call(this)
-      $app.$log('enhancePage', '#onShow complete')
+  const originOnShow = options.onShow
+  /**
+   * Intercept the original `onShow` method, and wait for `onLoad` to complete before executing it
+   */
+  const onShow: MP.Page.ILifetime['onShow'] = async function (this: any) {
+    // polling until `onLoad` complete
+    if (!onLoadComplete) {
+      setTimeout(() => {
+        onShow()
+      }, 100)
+      return
     }
+    const $app = getApp<IApp>()
+
+    $app.$log('enhancePage', '#onShow')
+    await originOnShow?.call(this)
+    $app.$log('enhancePage', '#onShow complete')
   }
 
   options = {
@@ -94,6 +98,7 @@ export default function enchangePage<
       ...options.data,
     },
     onLoad,
+    onShow,
   }
 
   Page<T, U>(options)
